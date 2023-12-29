cea174195b9bf40de01f334d12020f47774250f8
kkrolikowski <kkrolikowski.1993@gmail.com>
2023-12-29 21:10:41 +0100

    feat: add user entity inheritance adnotations

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,6 +9,9 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\InheritanceType("SINGLE_TABLE")]
+#[ORM\DiscriminatorColumn(name: "type", type: "string")]
+#[ORM\DiscriminatorMap(["user" => "User", "google_user" => "GoogleUser"])]
 #[ORM\Table(name: '`user`')]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
