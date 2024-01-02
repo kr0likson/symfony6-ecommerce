21e106d676a4a4c0e2c9ad4f02a47b50486e4b79
kkrolikowski <kkrolikowski.1993@gmail.com>
2024-01-02 21:28:51 +0100

    feat: configure admin edit form fields for category

--- src/Controller/Admin/ProductCategoryCrudController.php
+++ src/Controller/Admin/ProductCategoryCrudController.php
@@ -4,8 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\ProductCategory;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ProductCategoryCrudController extends AbstractCrudController
@@ -15,14 +14,11 @@ class ProductCategoryCrudController extends AbstractCrudController
         return ProductCategory::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            AssociationField::new('parent'),
         ];
     }
-    */
 }
