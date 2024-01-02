45fcfa5eea7eb7483672662288d326c3a2c07421
kkrolikowski <kkrolikowski.1993@gmail.com>
2024-01-02 21:27:11 +0100

    feat: pass products list to homepage template

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
+        $products = $this->entityManager->getRepository(Product::class)->findAll();
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'products' => $products,
         ]);
     }
 }
