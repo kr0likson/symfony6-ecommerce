666b8bb623ea120747a91184052d3525665eba8c
kkrolikowski <kkrolikowski.1993@gmail.com>
2024-01-08 23:20:36 +0100

    feat: add order entity

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\OrderRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: OrderRepository::class)]
@@ -18,9 +20,26 @@ class Order
     #[ORM\JoinColumn(nullable: false)]
     private ?User $customer = null;
 
+    #[ORM\OneToMany(mappedBy: 'relatedOrder', targetEntity: OrderItem::class, cascade: ['persist'])]
+    private Collection $orderItem;
+
     #[ORM\OneToOne(cascade: ['persist', 'remove'])]
     #[ORM\JoinColumn(nullable: false)]
-    private ?Cart $cart = null;
+    private ?Address $address = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastName = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $paymentStatus = null;
+
+    public function __construct()
+    {
+        $this->orderItem = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -39,14 +58,80 @@ class Order
         return $this;
     }
 
-    public function getCart(): ?Cart
+    /**
+     * @return Collection<int, OrderItem>
+     */
+    public function getOrderItem(): Collection
     {
-        return $this->cart;
+        return $this->orderItem;
     }
 
-    public function setCart(Cart $cart): static
+    public function addOrderItem(OrderItem $orderItem): static
     {
-        $this->cart = $cart;
+        if (!$this->orderItem->contains($orderItem)) {
+            $this->orderItem->add($orderItem);
+            $orderItem->setRelatedOrder($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOrderItem(OrderItem $orderItem): static
+    {
+        if ($this->orderItem->removeElement($orderItem)) {
+            // set the owning side to null (unless already changed)
+            if ($orderItem->getRelatedOrder() === $this) {
+                $orderItem->setRelatedOrder(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getAddress(): ?Address
+    {
+        return $this->address;
+    }
+
+    public function setAddress(Address $address): static
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): static
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): static
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getPaymentStatus(): ?string
+    {
+        return $this->paymentStatus;
+    }
+
+    public function setPaymentStatus(?string $paymentStatus): static
+    {
+        $this->paymentStatus = $paymentStatus;
 
         return $this;
     }
